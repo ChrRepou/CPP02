7738f4717e23bef6d50cacab5371f2fa10de3323
Christina Repou <crepou@3-F-13.42heilbronn.de>
2023-08-11 17:29:06 +0200

    fixing float constructor func, cerating main.c file

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: crepou <crepou@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 20:02:29 by crepou            #+#    #+#             */
-/*   Updated: 2023/08/11 17:05:41 by crepou           ###   ########.fr       */
+/*   Updated: 2023/08/11 17:27:28 by crepou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->fixedPointVal);
 }
 
@@ -58,24 +57,22 @@ Fixed::Fixed(const int num)
 Fixed::Fixed(const float num)
 {
 	std::cout << "Float constructor called" << std::endl;
-	int		scaled_val = static_cast<int>(num * 256); //256 = 2^8 --> 8 fractional bits
+	float	scaled_val = num * 256; //256 = 2^8 --> 8 fractional bits
 	int		rounded_num = roundf(scaled_val); // rounded to get the main part of num before the .
-	int		fractional_part = roundf((scaled_val - rounded_num) * 256); //get fractional part
-	int		fixed_point_num = (rounded_num << this->nbFractionalBits) | fractional_part; //combine main part and fractional part
-	setRawBits(fixed_point_num); 
+	setRawBits(rounded_num);
 }
 
 float Fixed::toFloat( void ) const
 {
-	int		main_part = this->fixedPointVal >> this->nbFractionalBits;
-	int		fractional_part = this->fixedPointVal & ((1 << this->nbFractionalBits) - 1);
+	int		main_part = getRawBits() >> this->nbFractionalBits;
+	int		fractional_part = getRawBits() & 255;
 	float	fnum = static_cast<float>(main_part) + static_cast<float>(fractional_part) / 256;
 	return	(fnum);
 }
 
 int Fixed::toInt( void ) const
 {
-	return (this->fixedPointVal >> this->nbFractionalBits);
+	return (getRawBits() >> this->nbFractionalBits);
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& obj)
